532da2499ee462c7b372729a61a97ee33b1d56a5
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2019-04-25 18:21:58 +0900

    소켓 옵션 쓰는법

---
+++ socket/socketOption/sock_type.c
@@ -0,0 +1,46 @@
+//家南 可记 函版
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+void error_handling(char *message);
+
+int main(int argc, char *argv[]) 
+{
+	int tcp_sock, udp_sock;
+	int sock_type;
+	int sock_sndbuf;
+	socklen_t optlen;
+	int state;
+	
+	optlen=sizeof(sock_type);
+	tcp_sock=socket(PF_INET, SOCK_STREAM, 0);
+	udp_sock=socket(PF_INET, SOCK_DGRAM, 0);	
+	printf("SOCK_STREAM: %d \n", SOCK_STREAM);
+	printf("SOCK_DGRAM: %d \n", SOCK_DGRAM);
+	
+	state=getsockopt(tcp_sock, SOL_SOCKET, SO_TYPE, (void*)&sock_type, &optlen);
+	if(state)
+		error_handling("getsockopt() error!");
+	printf("Socket type one: %d \n", sock_type);
+	
+	state=getsockopt(udp_sock, SOL_SOCKET, SO_TYPE, (void*)&sock_type, &optlen);
+	if(state)
+		error_handling("getsockopt() error!");
+	printf("Socket type two: %d \n", sock_type);
+
+	getsockopt(tcp_sock, SOL_SOCKET, SO_SNDBUF, (void*)&sock_sndbuf, &optlen);
+	printf("Socket send buf size : %d \n", sock_sndbuf);
+
+	//set
+
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
